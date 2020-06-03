59fb277236352b50f3c64a8839aca0af18d0496d
Filipozzi Alexis <alexis.filipozzi@gmail.com>
2020-06-03 09:08:10 +0200

    sample VS project

---
+++ ConsoleApp-VisualStudio/ConsoleApp-VisualStudio.cpp
@@ -0,0 +1,15 @@
+// ConsoleApp-VisualStudio.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, world, from Visual Studio! Press kkey to exit" << endl;
+	cin.ignore();
+    return 0;
+}
+
